dbedacca8eb1b92d67c857d5fa65069091953b6f
Zorica Brkic <zoricabrkic96@gmail.com>
2019-04-02 03:27:19 +0200

    Kreiran model baze, projekat za rest service i izgenerisane klase uz pomoc entity framework-a

---
+++ restservice/AutoSalonRestService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AutoSalonRestService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ restservice/AutoSalonRestService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutoSalonRestService.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
