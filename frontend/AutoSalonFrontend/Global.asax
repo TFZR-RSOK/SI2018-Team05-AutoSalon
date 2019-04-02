95e4d507a9205d587f38d6463dbd3777b51d44a4
Nemanja Tasic <ntasic89@gmail.com>
2019-04-02 03:38:55 +0200

    Inicijalizovan Web Forms projekat

---
+++ frontend/AutoSalonFrontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoSalonFrontend.Global" Language="C#" %>
---
+++ frontend/AutoSalonFrontend/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoSalonFrontend.Startup))]
+namespace AutoSalonFrontend
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
